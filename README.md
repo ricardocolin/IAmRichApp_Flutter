86d8726e53fff73790851725547716895a2a775c
Ricardo Colin <ricardochuche@gmail.com>
2022-03-07 16:15:51 -0800

    updated app with readme

--- README.md
+++ README.md
@@ -1,16 +1,11 @@
-# i_am_rich
+# I Am Rich App - Flutter
 
-I am rich app V1
+## Application Demo
 
-## Getting Started
+![alt text](images/application_demo.gif)
 
-This project is a starting point for a Flutter application.
+This project is a simple flutter application related to a famous app. 
 
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
-
-For help getting started with Flutter, view our
-[online documentation](https://flutter.dev/docs), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+## Environment Details
+- IDE: Android Studios
+- Language: Dart/Flutter
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,26 @@
+import 'package:animated_widgets/widgets/scale_animated.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.blueGrey,
+        backgroundColor: Colors.deepOrangeAccent.shade200,
         appBar: AppBar(
-          title: Text('I Am Rich'),
-          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Rich App'),
+          backgroundColor: Colors.black54,
         ),
-        body: const Center(
-          child: Image(
-            image: AssetImage('images/diamond.png'),
+        body: Center(
+          child: ScaleAnimatedWidget.tween(
+            enabled: true,
+            duration: Duration(milliseconds: 4000),
+            scaleDisabled: 0.5,
+            scaleEnabled: 1,
+            child: Container(
+              child: Image(
+                image: AssetImage('images/diamond.png'),
+              ),
+            ),
           ),
         ),
       ),
